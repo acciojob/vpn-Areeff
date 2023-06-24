9096a7701966661eeb8446ab10d68ec121eb22ab
Areeff <p.areeff786@gmail.com>
2023-06-24 18:36:22 +0530

    some implementation done

--- src/main/java/com/driver/model/User.java
+++ src/main/java/com/driver/model/User.java
@@ -13,7 +13,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String userName;
+    private String username;
     private String password;
     private String originalIp;
     private String maskedIp;
@@ -37,12 +37,12 @@ public class User {
         this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
